8690600ad60a706a15bc6a502927ee5ccb7c5450
ISPO0122217 <ISPO0122217@CL326-08>
2023-11-13 13:22:54 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //тип_переменной имя;
+            //тип_переменной имя = значение;
+            //тип_переменной: string (строка), int (целый тип), double (вещественный), bool (логический тип со значениями  true/false)            
+            //  Console.WriteLine("введите 2 числа для сложения:");
+            //string tmp = Console.ReadLine();
+            //int b = Int32.Parse(tmp);
+            //tmp = Console.ReadLine();
+            //int c = Convert.ToInt32(tmp);
+            //int sum = b + c;
+            // Console.WriteLine("Сумма двух чисел = " + sum);
+
+            //Console.WriteLine("Введите сторону квадрата:");
+            //string a = Console.ReadLine(); ;
+            //int b = Int32.Parse(a);
+            //int c = b * 4;
+            //Console.WriteLine("Площадь квадрата = " + c);
+
+            //Console.WriteLine("Введите стороны прямоугольника:");
+            //string a = Console.ReadLine();
+            //string b = Console.ReadLine();
+            //int c = Int32.Parse(a);
+            //int d = Int32.Parse(b);
+            //int S = c * d;
+            //int P = 2 * (c + d);
+            //Console.WriteLine("Площадь прямоугольника = " + S);
+            //Console.WriteLine("Периметр прямоугольника = " + P);
+
+            Console.WriteLine("Введите 2 переменных");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int c = Int32.Parse(a);
+            int d = Int32.Parse(b);
+            int e = d;
+            int c = d;
+
+
+
+
+
+
+        }
+    }
+}
