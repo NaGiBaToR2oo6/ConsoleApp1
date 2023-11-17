e5decfff3306d435842ac89fd958d71982997273
ISPO0122217 Миронов Иван Иванович <ISPO0122217@sibupk.su>
2023-11-18 01:40:51 +0700

    Сделал все задания

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Channels;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,7 @@ namespace ConsoleApp1
     {
         static void Begin1()
         {
-            Console.WriteLine("введите 2 числа для сложения:");
+            Console.WriteLine("Введите 2 числа для сложения:");
             string tmp = Console.ReadLine();
             int b = Int32.Parse(tmp);
             tmp = Console.ReadLine();
@@ -39,6 +40,59 @@ namespace ConsoleApp1
             Console.WriteLine("Периметр прямоугольника = " + P);
         }
 
+        static void Begin5()
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            string a = Console.ReadLine();
+            double b = Convert.ToDouble(a);
+            double V = Math.Pow(b, 3);
+            double S = Math.Pow(b, 2) * 6;
+            Console.WriteLine("Обьём куба = " + V);
+            Console.WriteLine("Площадь поверхности = " + S);
+        }
+
+        static void Begin7()
+        {
+            Console.WriteLine("Введите радиус");
+            string a = Console.ReadLine();
+            double R = Convert.ToDouble(a);
+            double L = 2 * Math.PI * R;
+            double S = Math.PI * Math.Pow(R, 2);
+            Console.WriteLine("Длина окружности = " + L);
+            Console.WriteLine("Площадь круга = " + S);
+        }
+
+        static void Begin9()
+        {
+            Console.WriteLine("Введите 2 числа");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            double c = Convert.ToDouble(a);
+            double d = Convert.ToDouble(b);
+            double e = Math.Sqrt(c * d);
+            Console.WriteLine("Среднее геометрическое = " + e);
+
+
+        }
+
+        static void Begin10()
+        {
+            Console.WriteLine("Введите 2 числа");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            double c = Convert.ToDouble(a);
+            double d = Convert.ToDouble(b);
+            double summ = c + d;
+            double diff = c - d;
+            double prod = Math.Pow(c, 2) * Math.Pow(d, 2);
+            double quo = Math.Pow(c, 2) / Math.Pow(d, 2);
+            Console.WriteLine("Сумма квадратов = " + summ);
+            Console.WriteLine("Разность квадратов = " + diff);
+            Console.WriteLine("Произведение квадратов = " + prod);
+            Console.WriteLine("Частное квадратов = " + quo);
+
+        }
+
         static void Begin22()
         {
 
@@ -54,46 +108,90 @@ namespace ConsoleApp1
             Console.WriteLine($"{a} {b}");
         }
 
-        static void Main(string[] args)
+        static void if2()
         {
-            //тип_переменной имя;
-            //тип_переменной имя = значение;
-            //тип_переменной: string (строка), int (целый тип), double (вещественный), bool (логический тип со значениями  true/false)            
+            Console.WriteLine("Введите число");
+            int a = Int32.Parse(Console.ReadLine());
 
-            //Begin1();
-            //Begin2();
-            //Begin3();
-            Begin22();
-
-            int a = 1, b=2;
-            //== - сравнение
-            //> < больше, меньше
-            //>= <=
-            // != не равно
-
-            // && -логическое и
-            // || 
-            
-            if( a == b)
+            if (a > 0)
             {
-                //
-            }
-            else if( a> b )
-            {
-
+                Console.WriteLine(a + 1);
             }
             else
             {
-
+                Console.WriteLine(a - 2);
             }
+        }
 
+        static void if4()
+        {
+            Console.WriteLine("Введите 3 числа");
 
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            int c = Int32.Parse(Console.ReadLine());
+            int i = 0;
 
+            if (a > 0) ++i;
+            if (b > 0) ++i;
+            if (c > 0) ++i;
+            Console.WriteLine("Количество положительных чисел" + i);
+        }
 
+        static void if7()
+        {
+            Console.WriteLine("Введите 2 числа");
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
 
+            if (a > b)
+            {
+                Console.WriteLine("Второе число меньше");
+            }
+            else if (b > a)
+            {
+                Console.WriteLine("Первое число меньше");
+            }
+            else
+            {
+                Console.WriteLine("Числа равны");
+            }
+        }
 
+        static void if9()
+        {
+            Console.WriteLine("Введите 2 числа");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double d = Convert.ToDouble(Console.ReadLine());
+            double A = 0;
+            double B = 0;
+            if (c > d)
+            {
+                A = d;
+                B = c;
+                Console.WriteLine("Число A= " + A);
+                Console.WriteLine("Число B= " + B);
+            }
+            else if (c < d)
+            {
+                A = c;
+                B = d;
+                Console.WriteLine("Число A = " + A);
+                Console.WriteLine("Число B = " + B);
+            }
+            else
+            {
+                Console.WriteLine("Числа равны ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+         
 
 
         }
     }
 }
+
