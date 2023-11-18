67853f96aa78982b35653a3aea93af200e4c8370
ISPO0122217 Миронов Иван Иванович <ISPO0122217@sibupk.su>
2023-11-18 18:04:38 +0700

    Ничего не сделал

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static string Zadanie(int a)
+        {
+
+            string result = "";
+            int unit = a % 10;
+            int tens = a % 100;
+            if (unit == 1 & tens != 11)
+            {
+                result = $"{a} рубль";
+            }
+            else if (unit > 1 & unit < 5 & tens != 12)
+
+            {
+                result = $"{a} рубля";
+            }
+            else
+            {
+
+                result = $"{a} рублей";
+
+            }
+            return result;
+        }
+            static void Main(string[] args)
+        {
+
+            Console.WriteLine(Zadanie(19));
+            Console.WriteLine(Zadanie(13));
+
+           
+
+        }
+    }
+}
